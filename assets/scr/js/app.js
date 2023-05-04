4c5206d85789b7589114432d86684ae50a7ed84d
denisbespyatov <denis@bespyatov.ru>
2023-05-04 10:51:23 +0500

    May 4
    current updaes

--- assets/scr/js/app.js
+++ assets/scr/js/app.js
@@ -2,7 +2,9 @@ function colapsSidebar() {
     let elem = document.querySelector('.sidebar');
     if (elem.classList.contains('collapsed')) {
         elem.classList.remove('collapsed');
+        window.localStorage.setItem('sidebarval', '')
     } else {
         elem.classList.add('collapsed');
+        window.localStorage.setItem('sidebarval', 'collapsed')
     }
 }
