d28bca5e701d529cbda63ff2f110e51f912e8ce3
denisbespyatov <denis@bespyatov.ru>
2023-09-04 12:52:00 +0500

    v1.0.1.7
    v1.0.1.7

--- widgets/Menu---.php
+++ widgets/Menu---.php
@@ -11,6 +11,7 @@ class Menu extends \yii\widgets\Menu
     public $items;
 
 
+
     public function run()
     {
         $menuItem = $this->items;
@@ -38,24 +39,20 @@ class Menu extends \yii\widgets\Menu
         return $html;
     }
 
-    protected function item($value, $key)
+    protected function item($value , $key)
     {
         $html = '<li class="sidebar-item">';
         $url = Url::to();
         $urlTo = is_array($value['url']) ? Url::to($value['url']['0']) : Url::to($value['url']);
-
-        if (!empty($value['activeClass'])) $addClass = ' ' . $value['activeClass']; else $addClass = '';
-
         if ($url == $urlTo) {
             $html = '<li class="sidebar-item active">';
         } else {
-            $html = '<li class="sidebar-item'.$addClass.'">';
+            $html = '<li class="sidebar-item">';
         }
-
-
+        
         if (array_key_exists('items', $value)) {
-            $html = $html . Html::a($value['icon'] . '<span class="align-middle">' . $value['label'] . '</span>', '#sidBar' . $key, ['class' => "sidebar-link collapsed", 'data-bs-toggle' => "collapse"]);
-            $html = $html . $this->items($value['items'], 'sidBar' . $key);
+            $html = $html . Html::a($value['icon'] . '<span class="align-middle">' . $value['label'] . '</span>', '#sidBar'. $key, ['class' => "sidebar-link collapsed", 'data-bs-toggle' => "collapse"]);
+            $html = $html . $this->items($value['items'], 'sidBar'. $key);
         } else {
             $html = $html . Html::a($value['icon'] . '<span class="align-middle">' . $value['label'] . '</span>', $urlTo, ['class' => "sidebar-link"]);
         }
@@ -64,9 +61,9 @@ class Menu extends \yii\widgets\Menu
         return $html;
     }
 
-    protected function items($value, $id)
+    protected function items($value , $id)
     {
-        $html = '<ul id="' . $id . '" class="sidebar-dropdown list-unstyled collapse " data-bs-parent="#sidebar">';
+        $html = '<ul id="'. $id. '" class="sidebar-dropdown list-unstyled collapse " data-bs-parent="#sidebar">';
         $url = Url::to();
         foreach ($value as $key => $item) {
             $urlTo = is_array($item['url']) ? Url::to($item['url']['0']) : Url::to($item['url']);
@@ -76,7 +73,7 @@ class Menu extends \yii\widgets\Menu
                 $html = $html . '<li class="sidebar-item">';
             }
             $html = $html . Html::a($item['label'], $urlTo, ['class' => "sidebar-link"]);
-            $html = $html . '</li>';
+            $html = $html .'</li>';
         }
 
         $html = $html . '</ul>';
